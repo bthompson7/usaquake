8fda706f10b2cc893529316cc03252061cd491f6
Ben Thompson <35780502+bthompson7@users.noreply.github.com>
2026-01-19 09:50:35 -0500

    Update jxmapviewer2 to 2.8.1, update tests

--- src/test/java/com/usaquake/maven/quickstart/Test_EartquakeDataFetch.java
+++ src/test/java/com/usaquake/maven/quickstart/Test_EartquakeDataFetch.java
@@ -2,24 +2,25 @@ package com.usaquake.maven.quickstart;
 
 import java.util.List;
 
-import data.FetchEQData;
+import data.DataService;
 import junit.framework.TestCase;
+import log.AppLog;
 import model.Earthquake;
+import ui.App;
 
 public class Test_EartquakeDataFetch extends TestCase  {
 
 	
 	//test that we can fetch earthquake data
 	public void testFetch() {
-		FetchEQData fetch = new FetchEQData();
-		try {
-			List<Earthquake> quakes = fetch.fetchData();
-			System.out.println(quakes.size());
-			assertTrue(quakes.size() > 0);
+		DataService fetch = new DataService();
+		AppLog logFile = new AppLog();
 
+		try {
+			List<Earthquake> quakes = fetch.getListOfEarthquakes(logFile);
+			assertFalse(quakes.isEmpty());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logFile.logError(e.getMessage());
 		}
 		
 	}
